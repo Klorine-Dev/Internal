885a7c3aa5d321ee01fa2b0dab99b700bb2efd8e
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2025-02-23 19:07:18 -0500

    added health script

--- godotLearning-SimplePlatformer/main.gd
+++ godotLearning-SimplePlatformer/main.gd
@@ -1,15 +1,40 @@
 extends Node3D
 
 @export var MovementEnabled: bool=true;
+@export var PlayerHealth: int=100; #$Player/PlayerHealth.Health
 
 func _ready()->void:
 	$UI/RespawnScreen.hide();
 	$UI.hide();
 	
+func die()->void:
+	# get_tree().reload_current_scene();
+	$UI/RespawnScreen.show();
+	$UI.show();
+	MovementEnabled=false;
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE);
+
+var delta: float
+func _process(_delta:float)->void:
+	# PlayerHealth=$Player/PlayerHealth.Health;
+	if(PlayerHealth<=0):die();
+	delta=_delta;
+
+# func SetPlayerHealth(amt:int)->void:
+# 	if PlayerHealth-amt>=0:PlayerHealth=amt;
+# 	else:die();
+@export var WhilePlayerTouching:bool=false;
 func _on_area_3d_body_entered(body:Node3D)->void:
 	if(body.name=="Player"):
-		# get_tree().reload_current_scene();
-		$UI/RespawnScreen.show();
-		$UI.show();
-		MovementEnabled=false;
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE);
+		WhilePlayerTouching=true;
+		$Player/DamageTimer.start();
+		PlayerHealth-=20;
+		# die();
+
+func _on_killbrick_collision_body_exited(body:Node3D) -> void:
+	if(body.name=="Player"):
+		WhilePlayerTouching=false;
+
+
+func _on_player_hit() -> void:
+	PlayerHealth-=20;

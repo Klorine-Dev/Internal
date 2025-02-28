f501d72890d1afb3541c88fb85c9867fbd0e7474
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2025-02-27 21:28:13 -0500

    debug panel + other qol stuff

--- godotLearning-Fps/Scripts/player.gd
+++ godotLearning-Fps/Scripts/player.gd
@@ -1,15 +1,17 @@
-#https://www.youtube.com/watch?v=qzncNwdSIkA at 10:39
 extends CharacterBody3D;
 
+# movement vars
 @export var SPEED:float=4.0;
 @export var SPRINT_MULTIPLIER:float=1.65;
 @export var JUMP_VELOCITY:float=4.5;
 @export var ANIMATION_PLAYER:AnimationPlayer;
 @export var CROUCH_SHAPECAST:ShapeCast3D;
+@export var TOGGLE_CROUCH:bool=false;
 @export_range(5,10,.1) var CROUCH_SPEED:float=7.0;
 var _is_crouching:bool=false;
 
-@export var MOUSE_SENSITIVITY:float=.5;
+# camera vars
+@export var MOUSE_SENSITIVITY:float=.75;
 @export var TILT_LOWER_LIMIT:=deg_to_rad(-90.0);
 @export var TILT_UPPER_LIMIT:=deg_to_rad(90.0);
 @export var CAMERA_CONTROLLER:Camera3D;
@@ -25,10 +27,22 @@ func _ready()->void:
 	CROUCH_SHAPECAST.add_exception($".");
 
 func _input(event: InputEvent)->void:
-	if(event.is_action_pressed("exit")):get_tree().quit();
-	if(event.is_action_pressed("crouch")):toggle_crouch();
+	if(event.is_action_pressed("exit")):
+		if(Input.mouse_mode==Input.MOUSE_MODE_CAPTURED):
+			Input.mouse_mode=Input.MOUSE_MODE_VISIBLE;
+		else:get_tree().quit();
+	#!godot is gstinky
+	#if(event.is_action_pressed("crouch") and TOGGLE_CROUCH):toggle_crouch();
+	#if(event.is_action_pressed("crouch") and !_is_crouching and !TOGGLE_CROUCH):
+	#	crouching(true);
+	#if(event.is_action_pressed("crouch") and !TOGGLE_CROUCH):
+	#	crouching(false);
 
 func _unhandled_input(event:InputEvent)->void:
+	if(Input.mouse_mode==Input.MOUSE_MODE_VISIBLE):
+		if(event is InputEventMouseButton and 
+		Input.is_mouse_button_pressed(1)):
+			Input.mouse_mode=Input.MOUSE_MODE_CAPTURED;
 	_mouse_input=(event is InputEventMouseMotion and Input.get_mouse_mode()==Input.MOUSE_MODE_CAPTURED);
 	if _mouse_input:
 		_rotation_input=-event.relative.x*MOUSE_SENSITIVITY;
@@ -47,6 +61,7 @@ func _update_camera(delta):
 	_tilt_input=0.0;
 
 func _physics_process(delta: float) -> void:
+	Global.debug.add_prop("PlayerVelocity",velocity,2);
 	if not is_on_floor():velocity+=get_gravity()*delta;
 	_update_camera(delta);
 	if Input.is_action_just_pressed("move_jump") and is_on_floor():velocity.y=JUMP_VELOCITY;
@@ -70,11 +85,14 @@ func _physics_process(delta: float) -> void:
 	move_and_slide();
 
 func toggle_crouch():
-	pass
 	#!doesn't work >:c
-	# if _is_crouching and !CROUCH_SHAPECAST.is_colliding(): 
-	# 	ANIMATION_PLAYER.play("crouch",-1,CROUCH_SPEED*-1,true);
-	# elif not _is_crouching: ANIMATION_PLAYER.play("crouch",-1,CROUCH_SPEED);
+	if _is_crouching and !CROUCH_SHAPECAST.is_colliding():crouching(false);
+	elif not _is_crouching:crouching(true);
+
+func crouching(state:bool):
+	match state:
+		true:ANIMATION_PLAYER.play("crouch",0,CROUCH_SPEED);
+		false:ANIMATION_PLAYER.play("crouch",0,-CROUCH_SPEED,true);
 
 func _on_animation_player_animation_started(anim_name:StringName) -> void:
 	if anim_name=="crouch":_is_crouching=!_is_crouching;
